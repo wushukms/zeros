d187c7142c179cbc28fba4dc19a204f478eab1c7
unknown <safina1999@rambler.ru>
2018-09-16 13:51:04 +0300

    all_passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result = 0;
+  let copy = number;
+  while (copy >= 5){
+      copy = Math.trunc(copy/5);
+      result += copy;
+  }
+  return result;
 }
